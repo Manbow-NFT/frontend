d6f6c9ed9a9c23f7417a2cbfe67e4237176798f8
banr1 <banri.yanahama@gmail.com>
2023-03-18 17:24:15 +0900

    update contract address

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
 import { ChainId } from '@thirdweb-dev/sdk';
 
 export const CHAIN_ID = ChainId.Mumbai;
-export const CONTRACT_ADDRESS = '0xa073666213961e2C52A46c13e4e0Fe920fb073C5';
+export const CONTRACT_ADDRESS = '0x816292707c5edfF3db2284b59b0433848162babb';
